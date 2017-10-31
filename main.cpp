61c9e742c0b6e359347817dbb705c86a50dd093a
Daria Simonova <31595849+DASHULYA1337@users.noreply.github.com>
2017-10-31 19:29:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+
+bool numbers(int & n, int * array)
+{
+	string string;
+	getline(cin, string);
+	istringstream stream(string);
+	bool success = true;
+	for (int i = 0; i < n; ++i) {
+		if (!(stream >> array[i])) {
+			success = false;
+			break;
+		}
+	}
+	return success;
+}
+
+int main() {
+	int n ;
+	
+	int min;
+
+	string s;
+	getline(cin, s);
+	istringstream stream(s);
+	if (stream >> n) {
+	  int * array = new int [n];
+		if (numbers(n, array)) {
+	for (int i = 1; i < n; i++){
+		for (int j = i; (j>0) && (array[j - 1] > array[j]); j--)
+			swap(array[j - 1], array[j]);
+					}
+				}
+			
+			for (int i = 0; i < n; i++) {
+				cout << array[i] << " ";
+			}
+			
+		}
+		else {
+			cout << "An error has occured while reading input data.";
+		}
+	return 0;
+}
